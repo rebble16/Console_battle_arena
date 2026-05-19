299110b1f6a08ac3e0c8e06b1d75f14097a10160
rebble <112185124+rebble16@users.noreply.github.com>
2026-05-19 22:09:48 +0500

    Update Character.h

--- Console_RPG_fight/Character.h
+++ Console_RPG_fight/Character.h
@@ -10,17 +10,17 @@ const int WIDTH = 14;
 extern char MAP[HEIGHT][WIDTH];
 class Character {
 protected:
-    string name;//имя персонажа
-    string feature;//его особенность(преимущество)
-    char appearance;//оторажение на экране
-    int HP;//уровень здоровья
-    int damage;//сколько HP снимает ближняя атака
-    string weapon;//название оружия
-    int posX, posY;//позиция на арене
+    string name;//РёРјСЏ РїРµСЂСЃРѕРЅР°Р¶Р°
+    string feature;//РµРіРѕ РѕСЃРѕР±РµРЅРЅРѕСЃС‚СЊ(РїСЂРµРёРјСѓС‰РµСЃС‚РІРѕ)
+    char appearance;//РѕС‚РѕСЂР°Р¶РµРЅРёРµ РЅР° СЌРєСЂР°РЅРµ
+    int HP;//СѓСЂРѕРІРµРЅСЊ Р·РґРѕСЂРѕРІСЊСЏ
+    int damage;//СЃРєРѕР»СЊРєРѕ HP СЃРЅРёРјР°РµС‚ Р±Р»РёР¶РЅСЏСЏ Р°С‚Р°РєР°
+    string weapon;//РЅР°Р·РІР°РЅРёРµ РѕСЂСѓР¶РёСЏ
+    int posX, posY;//РїРѕР·РёС†РёСЏ РЅР° Р°СЂРµРЅРµ
 public:
     Character(string C = "Unknown", int h = 100, int d = 10, int x = 5, int y = 5, string f = "close combat", char ch = 'W',string w="sword");
     virtual ~Character() {}
-    //геттеры 
+    //РіРµС‚С‚РµСЂС‹ 
     string getName() const { return name; }
     int getHP() const { return HP; }
     int getDamage() const { return damage; }
@@ -29,20 +29,20 @@ public:
     string getWeapon()const { return weapon; }
     int getPosX()const { return posX; };
     int getPosY()const { return posY; };
-    //сеттеры
+    //СЃРµС‚С‚РµСЂС‹
     int setHP(int hp=0) { return HP=hp; };
-    //функционал
-    void move(int dx, int dy, Character& o);//передвижение по арене
-    bool inBorders(int d)const;//проверка на достижение границы
-    bool isOccupied(int y, int x, Character& o)const;//проверка на занятость клетки на арене,чтобы избежать столкновение двух бойцов
-    void toRun(Character& o, bool isUnderAttack);//отбежать, если враг имеет преимущество в ближнем бою
-    void toPursue(Character& o);//преследовать врага, если он слаб в ближнем бою
-    bool isAllowedToAttack(Character& o)const;//проверка достаточно ли близко подошел для ближней атаки
-    void showCloseAttack(Character& o)const;//отобразить ближнюю атаку на экране
-    void animateRemoteAttack(Character& o, char symb);//отобразить атаку на расстоянии
-    void showRemoteAttack(Character& o, int x, int y, char& symb);//отобразить позицию стрелы(пламенного шара) на данный момент
+    //С„СѓРЅРєС†РёРѕРЅР°Р»
+    void move(int dx, int dy, Character& o);//РїРµСЂРµРґРІРёР¶РµРЅРёРµ РїРѕ Р°СЂРµРЅРµ
+    bool inBorders(int d)const;//РїСЂРѕРІРµСЂРєР° РЅР° РґРѕСЃС‚РёР¶РµРЅРёРµ РіСЂР°РЅРёС†С‹
+    bool isOccupied(int y, int x, Character& o)const;//РїСЂРѕРІРµСЂРєР° РЅР° Р·Р°РЅСЏС‚РѕСЃС‚СЊ РєР»РµС‚РєРё РЅР° Р°СЂРµРЅРµ,С‡С‚РѕР±С‹ РёР·Р±РµР¶Р°С‚СЊ СЃС‚РѕР»РєРЅРѕРІРµРЅРёРµ РґРІСѓС… Р±РѕР№С†РѕРІ
+    void toRun(Character& o, bool isUnderAttack);//РѕС‚Р±РµР¶Р°С‚СЊ, РµСЃР»Рё РІСЂР°Рі РёРјРµРµС‚ РїСЂРµРёРјСѓС‰РµСЃС‚РІРѕ РІ Р±Р»РёР¶РЅРµРј Р±РѕСЋ
+    void toPursue(Character& o);//РїСЂРµСЃР»РµРґРѕРІР°С‚СЊ РІСЂР°РіР°, РµСЃР»Рё РѕРЅ СЃР»Р°Р± РІ Р±Р»РёР¶РЅРµРј Р±РѕСЋ
+    bool isAllowedToAttack(Character& o)const;//РїСЂРѕРІРµСЂРєР° РґРѕСЃС‚Р°С‚РѕС‡РЅРѕ Р»Рё Р±Р»РёР·РєРѕ РїРѕРґРѕС€РµР» РґР»СЏ Р±Р»РёР¶РЅРµР№ Р°С‚Р°РєРё
+    void showCloseAttack(Character& o)const;//РѕС‚РѕР±СЂР°Р·РёС‚СЊ Р±Р»РёР¶РЅСЋСЋ Р°С‚Р°РєСѓ РЅР° СЌРєСЂР°РЅРµ
+    void animateRemoteAttack(Character& o, char symb);//РѕС‚РѕР±СЂР°Р·РёС‚СЊ Р°С‚Р°РєСѓ РЅР° СЂР°СЃСЃС‚РѕСЏРЅРёРё
+    void showRemoteAttack(Character& o, int x, int y, char& symb);//РѕС‚РѕР±СЂР°Р·РёС‚СЊ РїРѕР·РёС†РёСЋ СЃС‚СЂРµР»С‹(РїР»Р°РјРµРЅРЅРѕРіРѕ С€Р°СЂР°) РЅР° РґР°РЅРЅС‹Р№ РјРѕРјРµРЅС‚
     virtual void Attack(Character& obj) = 0;
     virtual bool isOnSight(Character& obj) = 0;
     virtual void Character_info(Character& obj) = 0;
-    friend ostream& operator<<(ostream& os, Character& o);//вывести основную информацию о персонаже
+    friend ostream& operator<<(ostream& os, Character& o);//РІС‹РІРµСЃС‚Рё РѕСЃРЅРѕРІРЅСѓСЋ РёРЅС„РѕСЂРјР°С†РёСЋ Рѕ РїРµСЂСЃРѕРЅР°Р¶Рµ
 };
