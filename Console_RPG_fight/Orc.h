e905d3d50b0136fb0c0498abecbdbded937ba006
rebble <112185124+rebble16@users.noreply.github.com>
2026-05-19 22:07:36 +0500

    Add files via upload

--- Console_RPG_fight/Orc.h
+++ Console_RPG_fight/Orc.h
@@ -8,6 +8,7 @@ private:
 public:
 	Orc(string cl, int hp, int dam, int x, int y, string f, char a,string w,
 		string r="Grey orc", int b=20) :Character(cl, hp, dam, x, y, f, a,w), race(r), buff(b) { }
+	virtual ~Orc() {}
 	void Attack(Character& o)override;//виртуальна€ функци€ атаки(здесь происходит логика отнимани€ HP при ударе),т.к. добавл€етс€ доп.урон в зависимости от расы орка(физ.сила)
 	bool isOnSight(Character& o)override;//вирутальна€ функци€ позвол€юща€ обнаружить врага в поле зрени€(каждый класс имеет свою дальность)
 	void Character_info(Character& obj)override;//виртуальна€ функци€, выводит информацию об атаке:кто кого атаковал,тип атаки(вблизи), сколько HP снесла атака и сколько HP осталось у противника
